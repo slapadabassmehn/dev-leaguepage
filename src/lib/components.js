55c22a9ce605e0528bd2ac4ab8a16860f4229c29
slapadabassmehn <74695287+slapadabassmehn@users.noreply.github.com>
2023-10-04 12:27:31 -0500

    Update components.js

--- src/lib/components.js
+++ src/lib/components.js
@@ -5,7 +5,7 @@ import News from './News/index.svelte';
 import Awards from './Awards/Awards.svelte';
 import Rosters from './Rosters/Rosters.svelte';
 import Rivalry from './Rivalry/index.svelte';
-import Transactions from './Transactions/Transactions.svelte';
+//import Transactions from './Transactions/Transactions.svelte';
 import TransactionsPage from './Transactions/TransactionsPage.svelte';
 import MatchupsAndBrackets from './Matchups/MatchupsAndBrackets.svelte';
 import Pagination from './Pagination.svelte';
