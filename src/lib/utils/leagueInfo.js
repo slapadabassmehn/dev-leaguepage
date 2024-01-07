e83d8c46933c33196a883803db2e18ea2c1d9d34
Ma Sm <crazyostrichboy0@gmail.com>
2024-01-07 10:09:00 -0600

    added stats

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -9,6 +9,7 @@ export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VIT
 export const homepageText = `
   <p>This is a paragrpah to test the home page</p>
   <p> enter a new paragraphy</p?
+  <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRLcNR7ZESvYlgzpD6-_RXdZ-mpcDWZVtmWBDDiJ305GL3HX5nCT48JC-u3cFuAKsmnjdcEx8ZUctsk/pubhtml?widget=true&amp;headers=false" width="1100" height="480"></iframe>
 
 `;
 
